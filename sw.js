de0ea303d57418b2085ef502546008853f6709e5
oixc <jan.h.hofmann@googlemail.com>
2026-08-05 12:51:16 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'grocery-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Pass ntfy network requests straight through; cache app assets
+  if (e.request.url.includes('ntfy.sh')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
